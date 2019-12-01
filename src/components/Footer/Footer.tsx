eca4f2ab05926e3023397dfd558bebffd10ac9d0
mkhbragg <mkhbragg@gmail.com>
2019-11-30 20:02:42 -0500

    Styled footer component.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,35 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import logo from '../../assets/icons/block-shop-logo.svg';
-import Aux from '../hoc/Aux/Aux';
 import SocialMediaGroup from '../SocialMediaGroup/SocialMediaGroup';
+import classes from './Footer.module.scss';
 
-const footer = () => (
-    <Aux>
-        <div>
-            <img src={logo} alt="Block Shop" />
-            <span>Copyright Block Shop 2019</span>
-            <span>All Rights Reserved</span>
-        </div>
-        <div>
-            <div>
-                <span>About Us</span>
-                <span>Story</span>
-                <span>Contact</span>
-                <span>Blog</span>
-            </div>
-            <div>
-                <span>Customer Care</span>
-                <span>FAQ</span>
-                <span>Warranty</span>
-                <span>Returns</span>
-            </div>
-            <div>
-                <span>Follow Us</span>
-                <SocialMediaGroup />
-            </div>
-        </div>
-    </Aux>
-);
+class Footer extends Component<any, any> {
+    state = {
+        mobile: true
+    }
 
-export default footer;
+    componentDidMount() {
+        this.updateWindow();
+        // add resize event listener
+        window.addEventListener('resize', this.updateWindow.bind(this));
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindow.bind(this));
+    }
+
+    updateWindow () {
+        this.setState({ mobile: window.innerWidth <= 768 })
+    }
+
+    render () {
+        return (
+            <div className={classes.Footer}>
+                <div className={classes.LogoContainer}>
+                    <img src={logo} alt="Block Shop logo" />
+                    <div className={`${classes.FinePrint} ${classes.Large}`}>
+                        <p>Copyright Block Shop 2019</p>
+                        <p>All Rights Reserved</p>
+                    </div>
+                </div>
+                <div className={!this.state.mobile ? classes.Row : ''}>
+                    <div className={classes.Row}>
+                        <div className={classes.Section}>
+                            <p className={classes.Heading}>About Us</p>
+                            <p>Story</p>
+                            <p>Contact</p>
+                            <p>Blog</p>
+                        </div>
+                        <div className={classes.Section}>
+                            <p className={classes.Heading}>Customer Care</p>
+                            <p>FAQ</p>
+                            <p>Warranty</p>
+                            <p>Returns</p>
+                        </div>
+                    </div>
+                    <div className={classes.Section}>
+                        <p className={classes.Heading}>Follow Us</p>
+                        <SocialMediaGroup />
+                    </div>
+                </div>
+                <div className={`${classes.FinePrint} ${classes.Small}`}>
+                    <p>Copyright Block Shop 2019</p>
+                    <p>All Rights Reserved</p>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Footer;
